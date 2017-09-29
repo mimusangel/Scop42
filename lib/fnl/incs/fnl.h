5242f140c023cd3c078559a7ff6dabc328323e69
mgallo <gallo.marcantoine@gmail.com>
2017-09-29 11:18:02 +0200

    test and ressource file

--- lib/fnl/incs/fnl.h
+++ lib/fnl/incs/fnl.h
@@ -18,11 +18,19 @@ typedef struct	s_fnl
 t_fnl			*fnl_new(const char *path);
 void			fnl_offset(t_fnl *file, ssize_t offset);
 void			fnl_free(t_fnl **file);
+
 ssize_t			fnl_strlen(const char *str);
 char			*fnl_strcpy(const char *str);
 char			*fnl_strcat(const char *s0, const char *s1);
 ssize_t			fnl_getchar(const char *str, const char c, ssize_t start);
 char			*fnl_strsub(const char *str, ssize_t start, ssize_t end);
+
 int				fnl_next_line(t_fnl *file, char **str);
+
+int				fnl_strtoint(const char *str);
+double			fnl_strtodouble(const char *str);
+int				fnl_strstartwith(const char *str, const char *start);
+ssize_t			fnl_strcountchar(const char *str, const char c);
+ssize_t			fnl_strstr(const char *str, const char *search);
 
 #endif
--- sources/scop.c
+++ sources/scop.c
@@ -2,30 +2,36 @@
 #include <stdlib.h>
 #include <fnl.h>
 
+# include <stdio.h>
 int		main(int ac, char **av)
 {
-	t_fnl	*file;
-	char	*str;
+	char *s0 = "v 0.21 0.42 0.69";
+	char *s1 = "vn test";
+	int a = fnl_strstartwith(s0, "v ");
+	int b = fnl_strstartwith(s1, "v ");
+	printf("%d\n", a);
+	printf("%d\n", b);
+	ssize_t count = fnl_strcountchar(s0, ' ');
+	printf("%zd\n", count);
 
-	file = fnl_new("README.md");
-	if (file)
-	{
-		while (fnl_next_line(file, &str) > 0)
-		{
-			write(1, str, fnl_strlen(str));
-			write(1, "\n", 1);
-			free(str);
-			str = NULL;
-		}
-		fnl_offset(file, 0);
-		while (fnl_next_line(file, &str) > 0)
-		{
-			write(1, str, fnl_strlen(str));
-			write(1, "\n", 1);
-			free(str);
-			str = NULL;
-		}
-		fnl_free(&file);
-	}
+	int i = fnl_getchar(s0, ' ', 0) + 1;
+	double val = fnl_strtodouble(s0 + i);
+	printf("%s\n", s0 + i);
+	printf("val: %f\n", val);
+
+	i = i + fnl_getchar(s0 + i, ' ', 0) + 1;
+	val = fnl_strtodouble(s0 + i);
+	printf("%s\n", s0 + i);
+	printf("val: %f\n", val);
+
+	i = i + fnl_getchar(s0 + i, ' ', 0) + 1;
+	val = fnl_strtodouble(s0 + i);
+	printf("%s\n", s0 + i);
+	printf("val: %f\n", val);
+
+	char *s2 = "f 1//2 3//4 5//6";
+	char *s3 = "f 1/2/3 4/5/6 7/8/9";
+	printf("s2: %s\n", s2 + fnl_strstr(s2, "//"));
+	printf("s3: %s\n", s3 + fnl_strstr(s3, "//"));
 	return (0);
 }
